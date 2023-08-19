32d488ffdb7e90d8e5b05feaf1b6cdd9dec948c1
ri_yarm <thogusus@gmail.com>
2023-08-19 20:24:43 +0500

    add: console log

---
+++ src/lib/mock/consoleText.ts
@@ -0,0 +1,6 @@
+export const consoleText = {
+  text:
+    "Очень приятно, что ты здесь находишься.\n" +
+    "Если тебе интересна моя персона пиши мне на емейл: \x1b[33mthogusus@gmail.com\x1b[0m\n" +
+    "Или в телеграмм \x1b[34mhttps://t.me/ri_yarm\x1b[0m",
+};
