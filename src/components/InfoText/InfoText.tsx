3f05fa8151085e8983f04da0ca8c6bbb2eeffa0e
ri_yarm <thogusus@gmail.com>
2024-03-13 22:13:51 +0500

    fix: info text

--- src/components/InfoText/InfoText.tsx
+++ src/components/InfoText/InfoText.tsx
@@ -9,7 +9,7 @@ const InfoText = () => (
       {mainText.hello} <SpanSC>{mainText.name}</SpanSC>
     </TitleSC>
     <SubtitleSC>
-      Я <FrontendTextSC>{mainText.work}</FrontendTextSC> разработчик
+      Я <FrontendTextSC>{mainText.work}</FrontendTextSC>-разработчик
     </SubtitleSC>
     <DescriptionSC>{mainText.description}</DescriptionSC>
   </Trail>
